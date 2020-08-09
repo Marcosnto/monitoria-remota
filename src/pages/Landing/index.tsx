20e6c2cde5bae57cbf73491f6b10b598321999a0
marcos_nto@hotmail.com <marcos.santos@supplylabs.com.br>
2020-08-08 22:42:02 -0300

    Create first page: Landing

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import logoImg from "../../assets/images/logo.svg";
+import landing from "../../assets/images/landing.svg";
+
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+import "./styles.css";
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Logo do Proffy escrita Proffy"></img>
+          <h2>Sua plataforma de estudos online</h2>
+        </div>
+
+        <img
+          src={landing}
+          alt="Imagem com desenhos gráficos de 
+          pessoas utilizando dispositivos para acessar a plataforma"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="/" className="study">
+            <img src={studyIcon} alt="Icone de um livro aberto" />
+            Estudar
+          </a>
+
+          <a href="/" className="give-classes">
+            <img src={giveClassesIcon} alt="Icone de uma televisão" />
+            Dar aulas
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Icone de um coração roxo" />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
